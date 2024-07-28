d53eaf53004e0a5fc7e282838fcfdb7b0101a3ab
Johannes Brüderl <johannes.bruederl@gmail.com>
2024-07-28 22:04:48 +0200

    update

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,4 +1,5 @@
 require'nvim-treesitter.configs'.setup {
+  parser_install_dir = "$HOME/.cache/treesitter/parsers",
   highlight = {
     enable = true,
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
@@ -12,3 +13,4 @@ require'nvim-treesitter.configs'.setup {
   }
 }
 
+vim.opt.runtimepath:append("$HOME/.cache/treesitter/parsers")
